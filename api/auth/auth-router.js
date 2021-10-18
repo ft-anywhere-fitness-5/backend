5feeb99dcb7beb41c30923cce77dcfca1c23ae0b
jMisch44 <jennimisch@gmail.com>
2021-10-18 12:55:01 -0400

    auth register in progress

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,10 @@
+// const authController = require('./authController')
+// const { validateBody, checkUsernameExists } = require('./auth-middleware')
+
 const router = require('express').Router();
 
-router.post('/register', (req, res, next) => res.json('register'))
+// router.post('/register', validateBody, checkUsernameExists, (req, res, next) => authController.register)
 
-router.post('/login', (req, res, next) => res.json('login'))
+// router.post('/login', validateBody, checkUsernameExists, (req, res, next) => res.json('login'))
 
 module.exports = router;
--- api/auth/authController.js
+++ api/auth/authController.js
@@ -1,7 +1,14 @@
-// const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt')
+const Users = require('../users/users')
+
+const ROUNDS = process.env.ROUNDS || 8
 
 const authController = {
-
+    async register(req, res, next) {
+        req.body.password = await bcrypt.hash(req.body.password, parseInt(ROUNDS))
+        const { username, user_id } = await Users.addUser(req.body)
+        res.json({ username, user_id })
+    }
 }
 
 module.exports = authController
