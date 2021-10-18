f96c38caa0eeac213e3fced291fc4d9acd583e5d
jMisch44 <jennimisch@gmail.com>
2021-10-18 14:03:30 -0400

    register working, refactor to use authcontroller later

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,10 +1,20 @@
-// const authController = require('./authController')
-// const { validateBody, checkUsernameExists } = require('./auth-middleware')
+const Users = require('../users/users')
+const bcrypt = require('bcrypt')
+const { validateBody, checkUsernameExists } = require('./auth-middleware')
+const ROUNDS = process.env.ROUNDS || 8
 
 const router = require('express').Router();
 
-// router.post('/register', validateBody, checkUsernameExists, (req, res, next) => authController.register)
+router.post('/register', validateBody, checkUsernameExists, async (req, res, next) => {
+    try {
+        req.body.password = await bcrypt.hash(req.body.password, parseInt(ROUNDS))
+        const { username, user_id } = await Users.addUser(req.body)
+        res.json({ username, user_id })
+    } catch (err) {
+        next(err)
+    }
+})
 
-// router.post('/login', validateBody, checkUsernameExists, (req, res, next) => res.json('login'))
+router.post('/login', validateBody, checkUsernameExists, (req, res, next) => res.json('login'))
 
 module.exports = router;
--- api/auth/authController.js
+++ api/auth/authController.js
@@ -5,9 +5,9 @@ const ROUNDS = process.env.ROUNDS || 8
 
 const authController = {
     async register(req, res, next) {
-        req.body.password = await bcrypt.hash(req.body.password, parseInt(ROUNDS))
-        const { username, user_id } = await Users.addUser(req.body)
-        res.json({ username, user_id })
+        // req.body.password = await bcrypt.hash(req.body.password, parseInt(ROUNDS))
+        // const { username, user_id } = await Users.addUser(req.body)
+        // res.json({ username, user_id })
     }
 }
 
