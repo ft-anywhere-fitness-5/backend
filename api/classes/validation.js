c17fdda5a0424431a5cdd28ec30b48a04ad2c074
jMisch44 <jennimisch@gmail.com>
2021-10-19 18:39:30 -0400

    class validation change

--- api/classes/validation.js
+++ api/classes/validation.js
@@ -4,11 +4,11 @@ const classesSchema = Joi.object({
     class_name: Joi.string().trim().required(),
     class_type: Joi.string().trim().required(),
     class_start_time: Joi.string().trim().required(),
-    class_duration: Joi.number().positive().precision(0).required(),
+    class_duration: Joi.string().trim().required(),
     class_intensity: Joi.string().trim().required(),
     class_location: Joi.string().trim().required(),
-    class_registered_attendees: Joi.number().positive().required().default(0),
-    class_max_size: Joi.number().positive().precision(0).required(),
+    class_registered_attendees: Joi.number().positive().default(0),
+    class_max_size: Joi.string().trim().required(),
 })
 
 module.exports = classesSchema
