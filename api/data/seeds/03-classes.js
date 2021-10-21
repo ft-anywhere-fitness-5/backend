eabfed8c8800dcb52b1977615dcb9369ff3408d6
jMisch44 <jennimisch@gmail.com>
2021-10-21 16:09:47 -0400

    seeds data to all strings

--- api/data/seeds/03-classes.js
+++ api/data/seeds/03-classes.js
@@ -9,11 +9,11 @@ exports.seed = function(knex) {
           class_name: 'Intro to Yoga',
           class_type: 'Yoga',
           class_start_time: '10:30 a.m.',
-          class_duration: 30,
+          class_duration: '30',
           class_intensity: 'low',
           class_location: 'Park',
-          class_registered_attendees: 10,
-          class_max_size: 20,
+          class_registered_attendees: '10',
+          class_max_size: '20',
         }        
       ]);
     });
