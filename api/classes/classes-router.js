dfdd69c2fb786422853ce69557345b2489441771
jMisch44 <jennimisch@gmail.com>
2021-12-21 15:54:55 -0500

    changes string to a constant variable

--- api/classes/classes-router.js
+++ api/classes/classes-router.js
@@ -3,16 +3,18 @@ const { validateId, validateBody } = require('./classes-middleware')
 const { only, restricted } = require('../auth/auth-middleware')
 const router = require('express').Router();
 
+const INSTRUCTOR_ROLE = 'instructor'
+
 router.get('/', classesController.getAllClasses)
 
 router.get('/:id', validateId, classesController.getClassById)
+// add instructor id so i can make it so instructors can only edit their own classes 
+router.post('/', validateBody, restricted, only(INSTRUCTOR_ROLE), classesController.createClass)
 
-router.post('/', validateBody, restricted, only('instructor'), classesController.createClass)
 
+router.put('/:id', validateId, validateBody, restricted, only(INSTRUCTOR_ROLE), classesController.updateClass)
 
-router.put('/:id', validateId, validateBody, restricted, only('instructor'), classesController.updateClass)
-
-router.delete('/:id', validateId, restricted, only('instructor'), classesController.deleteClass)
+router.delete('/:id', validateId, restricted, only(INSTRUCTOR_ROLE), classesController.deleteClass)
 
 router.use('*', classesController.notFound)
 
