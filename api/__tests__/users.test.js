f6c21f14e4e9cadd264f292b21212be968507477
jMisch44 <jennimisch@gmail.com>
2021-12-21 15:56:29 -0500

    adds users test for post, get in progress, and todos on other endpoints

--- api/__tests__/users.test.js
+++ api/__tests__/users.test.js
@@ -14,5 +14,33 @@ afterAll(async () => {
 })
 
 describe('[GET] /', () => {
+    let res
+    beforeEach(async () => {
+        let client = await request(server).post('/api/auth/login').send({ username: 'Maine', password: 'Augusta' })
+    })
     it.todo('responds with an array of users classes')
+})
+
+describe('[POST] /', () => {
+    let res
+    let client
+    beforeEach(async () => {
+        client = await request(server).post('/api/auth/login').send({ username: 'Maine', password: 'Augusta' })
+        res = await request(server).post('/api/user/').set('authorization', client.body.token).send({ class_id: 1 })
+    })
+    it('responds with for a class', async () => {
+        console.log(res.body)
+    })
+    it('updates the database with the users class', async () => {
+        const usersClassEntry = db('users_classes').where({'user_id': client.id, 'class_id': res.class_id})
+        expect(usersClassEntry).toBeTruthy()
+    })
+})
+
+describe('[PUT] :id/', () => {
+    it.todo('update your registered class')
+})
+
+describe('[DELETE] /', () => {
+    it.todo('un-register for a class')
 })
